59f011518aa3da66e75b9829f4ef57bf380cc844
Vaani Verma <vermavaani887@gmail.com>
2026-07-19 22:43:43 +0530

    Add password generator script
    As part of my Python learning journey, I built this Password Generator to create strong and random passwords. The project allows users to generate secure passwords with customizable lengths and different character combinations. Building this project strengthened my Python programming skills and gave me hands-on experience with the random module and user input handling.

---
+++ Genrate_Password.py
@@ -0,0 +1,17 @@
+# Password Generator
+
+import random
+import string
+
+print("===== Password Generator =====")
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("\nGenerated Password:", password)
